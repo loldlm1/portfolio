26b1b44c99b5fc08f74664fd2d8117d62af560ec
loldlm1 <loldlm1@gmail.com>
2017-05-24 11:11:55 -0400

    link blank, fixed admin

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Project do
-  permit_params :name, :description, :link, :image_link, :user_id, :contribution
+  permit_params :name, :description, :contribution, :link, :image_link, :user_id
 end
